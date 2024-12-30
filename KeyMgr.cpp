f69c7d71062fa4df939659de3b7f74e7be80b360
moculll <823309460@qq.com>
2024-12-30 15:17:19 +0800

    fix stop listening failed issue

--- KeyMgr.cpp
+++ KeyMgr.cpp
@@ -13,7 +13,7 @@
 #else
 #define DEBUG_PRINT(fmt, ...)
 #endif
-KeyMgr::KeyMgr() : listening(false)
+KeyMgr::KeyMgr() : listening(false), listenerThreadId(0)
 {
     startListening();
 
@@ -48,11 +48,15 @@ LRESULT CALLBACK KeyMgr::keyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 
             auto it = keyBindings.find(currentKeys);
             if (it != keyBindings.end()) {
-                auto keyBinding = it->second;
-                auto newWork = std::async(std::launch::async, [keyBinding]() {
-                    keyBinding->invoke();
-                });
-                works.emplace_back(std::move(newWork));
+                auto keyBindingVector = it->second;
+                for (auto keyBinding : keyBindingVector) {
+                    auto newWork = std::async(std::launch::async, [keyBinding]() {
+                        keyBinding->invoke();
+                    });
+                    std::lock_guard<std::mutex> lock(KeyMgr::vecMutex);
+                    works.emplace_back(std::move(newWork));
+                }
+                
                 
             }
         }
@@ -65,26 +69,58 @@ void KeyMgr::startListening() {
     if (!listening) {
         listening = true;
         listenerFuture = std::async(std::launch::async, [this]() {
+            DEBUG_PRINT("KeyMgr start listening.");
             hHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, NULL, 0);
             if (hHook == NULL) {
                 DEBUG_PRINT("KeyMgr failed to install hook, check!");
                 throw std::runtime_error("Failed to install hook");
             }
+            listenerThreadId = GetThreadId(GetCurrentThread());
+            DEBUG_PRINT("thread id: %d", listenerThreadId);
             MSG msg;
             while (GetMessage(&msg, NULL, 0, 0)) {
+                if (msg.message == WM_QUIT) {
+                    DEBUG_PRINT("exit get message");
+                    break;
+                }
+                DEBUG_PRINT("exit get message");
                 TranslateMessage(&msg);
                 DispatchMessage(&msg);
                 
             }
         });
-        DEBUG_PRINT("KeyMgr start listening.");
+        
+
+        manageFuture = std::async(std::launch::async, [this]() {
+            DEBUG_PRINT("KeyMgr start managing, listening: %d", listening ? 1 : 0);
+            while (listening) {
+                for (auto it = works.begin(); it != works.end(); ) {
+                    DEBUG_PRINT("cycle in works.");
+                    if (it->wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+                        it->get();
+                        std::lock_guard<std::mutex> lock(KeyMgr::vecMutex);
+                        it = works.erase(it);
+                        DEBUG_PRINT("erased work.");
+                    }
+                    else {
+                        ++it;
+                    }
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+           
+        });
+        
+
     }
 }
 
 void KeyMgr::stopListening() {
     if (listening) {
         listening = false;
-        PostThreadMessage(GetCurrentThreadId(), WM_QUIT, 0, 0);
+
+        while (!listenerThreadId);
+        PostThreadMessage(listenerThreadId, WM_QUIT, 0, 0);
         if (listenerFuture.valid()) {
             listenerFuture.get();
         }
@@ -95,12 +131,13 @@ void KeyMgr::stopListening() {
                 }
             }
         }
-        if (listenerFuture.valid()) {
-            listenerFuture.get();
+        if (manageFuture.valid()) {
+            manageFuture.get();
         }
 
 
         DEBUG_PRINT("KeyMgr stop listening.");
+
     }
 }
 
@@ -117,6 +154,14 @@ int main(int argc, char* argv[])
         {
             DEBUG_PRINT("a: %d, b:%f, c:%lf, d:%d", a, b, c, d ? 1 : 0);
         }
+
+        void func_2(const std::shared_ptr<KeyMgr>& keyManager)
+        {
+            keyManager->bindKeys({ VK_F3 }, std::function<void()>([this]() {
+                DEBUG_PRINT("inside val: %d", insideVal);
+            }));
+        }
+
     private:
         int insideVal;
 
@@ -126,4 +171,10 @@ int main(int argc, char* argv[])
     keyManager->bindKeys({ VK_F1 }, std::function(test::func_0), testClass);
     keyManager->bindKeys({ VK_F2 }, std::function(test::func_1), 151, float(24841.614), 151.161165464, false);
 
+    testClass.func_2(keyManager);
+    
+    keyManager->bindKeys({ VK_F3 }, std::function<void()>([]() {
+        DEBUG_PRINT("outside val: %d", 1);
+    }));
+    while (1);
 }
